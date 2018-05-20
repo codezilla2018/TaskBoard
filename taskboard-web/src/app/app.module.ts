a4bbf925239aadb1751d64b5df8142d2d26943ff
Dhanuka Perera <dhanukashehanperera@gmail.com>
2018-05-20 10:17:43 +0530

    added angularfire2 and firebase dependencies to the project

--- taskboard-web/src/app/app.module.ts
+++ taskboard-web/src/app/app.module.ts
@@ -1,9 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { FIREBASE } from '../environments/firebase.config';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 @NgModule({
   declarations: [
@@ -12,7 +19,13 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(FIREBASE),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
