67081b62beac6058ce063af37b8c74b5adf256f6
Dhanuka Perera <dhanukashehanperera@gmail.com>
2018-05-24 07:21:47 +0530

    adding user to firebase

--- taskboard-web/src/app/create-user/create-user.component.ts
+++ taskboard-web/src/app/create-user/create-user.component.ts
@@ -1,9 +1,15 @@
+import { UserI } from './../models/user/user.interface';
 import { rolesList } from './../data/role.data';
 import { genderList } from './../data/gender.data';
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
+
+import { AngularFireDatabase ,AngularFireList} from 'angularfire2/database';
+import { FirebaseListObservable } from "angularfire2/database-deprecated";
+
+
 @Component({
   selector: 'app-create-user',
   templateUrl: './create-user.component.html',
@@ -11,7 +17,12 @@ import { map, startWith } from 'rxjs/operators';
 })
 export class CreateUserComponent implements OnInit {
 
-  constructor() { }
+  user = {} as UserI;
+ userRef$: AngularFireList<UserI>;
+
+  constructor(private database: AngularFireDatabase) {
+    this.userRef$ = this.database.list('/db/users/');
+   }
   
   ngOnInit() {this.filteredOptions = this.myControl.valueChanges
     .pipe(
@@ -33,6 +44,12 @@ export class CreateUserComponent implements OnInit {
   filter(val: string): string[] {
     return this.options.filter(option =>
       option.toLowerCase().includes(val.toLowerCase()));
+  }
+
+  createUser(){
+    console.log('add task');
+    console.log(this.user);
+    this.userRef$.push(this.user);
   }
 
 
--- taskboard-web/src/app/models/user/user.interface.ts
+++ taskboard-web/src/app/models/user/user.interface.ts
@@ -1,9 +1,8 @@
-import { Account } from './../ account/ account.interface';
-
 export interface UserI{
     $key?: string;
-    firstName:string,
-    lastName:string,
-    account:Account,
-    role:string
+    firstName?:string,
+    lastName?:string,
+    email?:string
+    role?:string,
+    gender?:string
 }
